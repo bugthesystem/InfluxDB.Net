12a8d8e274d1212914dd6a4ef5dd2c95db20680b
Ziya SARIKAYA <sarikayaziya@gmail.com>
2014-09-14 20:45:34 +0300

    Add missed file.

---
+++ src/InfluxDB.Net/Core/InfluxDbResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using RestSharp;
+
+namespace InfluxDB.Net.Core
+{
+    public class InfluxDbResponse
+    {
+        public IRestResponse Raw { get; private set; }
+
+        public bool Success
+        {
+            get { return Raw.StatusCode == HttpStatusCode.OK; }
+        }
+
+        public InfluxDbResponse(IRestResponse response)
+        {
+            Raw = response;
+        }
+    }
+}
